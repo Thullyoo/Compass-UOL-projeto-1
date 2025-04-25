a74d7a1da46487a287c8db7f5687dcdf419be744
Thullyoo <123775158+Thullyoo@users.noreply.github.com>
2025-04-25 15:56:44 -0300

    Update nginx_monitor.sh

--- nginx_monitor.sh
+++ nginx_monitor.sh
@@ -2,11 +2,13 @@
 
 DATA=$(date +%d/%m/%Y)
 HORA=$(date +%H:%M:%S)
-STATUS_FILE="/tmp/status_server"
-AVAILABLE="false"
 BOT_TOKEN=
 CHAT_ID=
+STATE_FILE="/tmp/server_status.state"
 
+if [ ! -f "$STATE_FILE" ]; then
+    echo "false" > "$STATE_FILE"
+fi
 
 function verify_nginx_available(){
 	systemctl is-active --quiet nginx
@@ -25,28 +27,31 @@ function verify_website(){
 
 function send_message_to_telegram(){
 	local message="$1"
-	curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+	curl -s -o /dev/null -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
 		-H 'Content-Type: application/json' \
 		-d "{\"chat_id\":\"$CHAT_ID\", \"text\":\"$message\"}"
 }
 
-if [ -f "$STATUS_FILE" ]; then
-	AVAILABLE=$(cat "$STATUS_FILE")
-else
-	AVAILABLE="true"
-fi
 
-if verify_nginx_available && verify_website; then
-	if [ "$AVAILABLE" == "false" ]; then
-		send_message_to_telegram "✅ O servidor está de volta! A página web está funcionando normalmente e disponível para acesso."
-		echo "true" > "$STATUS_FILE"
-	fi
-	echo "$DATA $HORA ** Serviço Nginx rodando e página web no ar!" >> /var/log/meu_script.log
-else
-	if [ "$AVAILABLE" == "true" ]; then
-		send_message_to_telegram "⚠️ Atenção: O servidor está enfrentando problemas e a página web não está disponível no momento. Estamos trabalhando para resolver o mais rápido possível."
-		echo "false" > "$STATUS_FILE"
-	fi
-	echo "$DATA $HORA ** Serviço Nginx não está rodando, página web fora do ar!" >> /var/log/meu_script.log
-fi
+
+while true; do
+    
+
+    AVAILABLE=$(cat "$STATE_FILE")
+
+    if verify_nginx_available && verify_website; then
+        if [ "$AVAILABLE" == "false" ]; then
+            send_message_to_telegram "✅ O servidor está de volta! A página web está funcionando normalmente e disponível para acesso."
+            echo "true" > "$STATE_FILE"
+        fi
+        echo "$DATA $HORA ** Serviço Nginx rodando e página web no ar!" >> /var/log/meu_script.log
+    else
+        if [ "$AVAILABLE" == "true" ]; then
+            send_message_to_telegram "⚠️ Atenção: O servidor está enfrentando problemas e a página web não está disponível no momento. Estamos trabalhando para resolver o mais rápido possível."
+            echo "false" > "$STATE_FILE"
+        fi
+        echo "$DATA $HORA ** Serviço Nginx não está rodando, página web fora do ar!" >> /var/log/meu_script.log
+    fi
+    sleep 60
+done
 
